8e9b819d23623b93b7e1c64d9975f3176c70237c
Mayur Neware <mayur.oct.95@gmail.com>
2023-05-09 19:40:28 +0530

    initial second commit

--- src/com/practiceapp/Main.java
+++ src/com/practiceapp/Main.java
@@ -7,5 +7,18 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello world");
+        Calculate calculate=new Calculate();
+        calculate.add();
+    }
+}
+
+class Calculate {
+
+    int n=10;
+    int m=20;
+
+    public void add() {
+        int c=n+m;
+        out.println(c);
     }
 }
