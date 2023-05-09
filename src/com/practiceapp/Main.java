0980f6efad9ac8a861b15911ee0463c8e5861742
Mayur Neware <mayur.oct.95@gmail.com>
2023-05-09 19:17:45 +0530

    initial first commite

---
+++ src/com/practiceapp/Main.java
@@ -0,0 +1,11 @@
+package com.practiceapp;
+
+import static java.lang.System.out;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello world");
+    }
+}
